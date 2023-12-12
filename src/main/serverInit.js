bbba315f63faab0d4c0c18cb4f35009880fdf7d2
JuanPabloDaBa99 <juanpidb68@gmail.com>
2023-12-12 15:30:34 +0100

    se añaden cors para peticiones externas

--- src/main/serverInit.js
+++ src/main/serverInit.js
@@ -10,7 +10,14 @@ const SEQUELIZE_HTTP = 'postgres://citizix_user:S3cret@localhost:5432/prueba';
 
 const server = Hapi.server({
     port : API_PORT,
-    host : 'localhost'
+    host : 'localhost',
+    routes: {
+        cors: {
+            origin: ["http://localhost:3000"],
+            headers: ["Accept", "Content-Type"],
+            additionalHeaders: ["X-Requested-With"]
+        }
+      }
 });
 
 async function init() {
